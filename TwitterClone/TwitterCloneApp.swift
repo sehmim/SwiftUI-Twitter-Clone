263e34c7068f936d5bb760701235cc265e8077c0
sehmim <sehmim.al@gmail.com>
2021-01-07 18:50:34 -0500

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Sehmim Haque on 2021-01-07.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
